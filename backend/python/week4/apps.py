646e03905da63a4e40f0a94dd0fbf0bb7804a4c0
kreeeesh17 <kreeshmodi.codeforces@gmail.com>
2026-03-21 18:34:55 +0530

    week4 - updated

--- backend/python/week4/apps.py
+++ backend/python/week4/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from .db_connection import initialize_mongo
 
 
 class Week4Config(AppConfig):
     name = "week4"
+
+    def ready(self):
+        initialize_mongo()
---
+++ backend/python/week4/db_connection.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from mongoengine import connect
+
+load_dotenv()
+
+
+def initialize_mongo():
+    mongo_user = os.getenv("MONGO_USER", "root")
+    mongo_pass = os.getenv("MONGO_PASS", "example")
+    mongo_host = os.getenv("MONGO_HOST", "localhost")
+    mongo_port = os.getenv("MONGO_PORT", "27019")
+    mongo_db = os.getenv("MONGO_DB", "interneers_lab_week4")
+
+    mongo_uri = (
+        f"mongodb://{mongo_user}:{mongo_pass}"
+        f"@{mongo_host}:{mongo_port}/{mongo_db}?authSource=admin"
+    )
+
+    connect(
+        db=mongo_db,
+        host=mongo_uri,
+        alias="default",
+    )
