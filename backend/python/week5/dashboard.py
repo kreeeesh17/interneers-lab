40ea1c66b70c479cced9d3e425f8283b216eaf89
kreeeesh17 <kreeshmodi.codeforces@gmail.com>
2026-03-31 00:32:06 +0530

    week5 - dashboard created

--- backend/python/week5/dashboard.py
+++ backend/python/week5/dashboard.py
@@ -35,7 +35,7 @@ def fetch_products(selected_category_id=None):
             "Price": product.price,
             "Brand": product.brand,
             "Quantity": product.quantity,
-            "Category": product.category.title
+            "Category": product.category.title if product.category else "No Category"
         })
 
     return pd.DataFrame(rows)
@@ -72,13 +72,19 @@ def inventory_table(category_id):
 # showing stock alert
 @st.fragment
 def stock_alert(df):
+
+
+def stock_alert(df):
     st.subheader("Stock Alert")
+
     if not df.empty:
-        low_stock_row = []
+        low_stock_rows = []
+
         for index, row in df.iterrows():
             if row["Quantity"] < 5:
-                low_stock_row.append(row)
-        low_stock_df = pd.DataFrame(low_stock_row)
+                low_stock_rows.append(row)
+
+        low_stock_df = pd.DataFrame(low_stock_rows)
 
         if not low_stock_df.empty:
             st.error("Some products are running low in stock")
@@ -89,5 +95,67 @@ def stock_alert(df):
         st.info("No products found in this category")
 
 
+# adding product
+@st.fragment
+def add_product(categories):
+    st.subheader("Add Product")
+    category_title = []
+    for category in categories:
+        category_title.append(category.title)
+
+    with st.form("add_product_form"):
+        new_name = st.text_input("Name")
+        new_description = st.text_area("Description")
+        new_price = st.number_input(
+            "Price", min_value=0.0, step=1.0, format="%.2f")
+        new_brand = st.text_input("Brand")
+        new_quantity = st.number_input(
+            "Quantity", min_value=0, step=1)
+        new_category_title = st.selectbox("Category", category_title if category_title else [
+            "No categories found "])
+        submitted = st.form_submit_button("Add Product")
+
+    if submitted:
+        if not new_name.strip():
+            st.error("Product name cannot be empty")
+        elif not new_description.strip():
+            st.error("Description cannot be empty")
+        elif not new_brand.strip():
+            st.error("Brand cannot be empty")
+        elif not new_category_title:
+            st.error("No category exist. Pls create a category first in week 4")
+        else:
+            selected_category = ProductCategory.objects(
+                title=new_category_title).first()
+            Product(name=new_name, description=new_description,
+                    price=Decimal(str(new_price)), brand=new_brand, quantity=int(new_quantity), category=selected_category).save()
+            st.success(f"Product '{new_name}' added successfully")
+            st.rerun()
+
+
+# removing product
+@st.fragment
+def remove_product():
+    st.subheader("Remove Product")
+    all_products = Product.objects().order_by("name")
+    product_options = {}
+    for product in all_products:
+        label = f"{product.name} (ID: {product.id})"
+        product_options[label] = product.id
+
+    if product_options:
+        selected_product_label = st.selectbox(
+            "Select Product to remove", list(product_options.keys()))
+    if st.button("Remove Product"):
+        selected_product_id = product_options[selected_product_label]
+        Product.objects(id=selected_product_id).delete()
+        st.success("Product removed successfully")
+        st.rerun()
+    else:
+        st.info("No products found")
+
+
 df = inventory_table(selected_category_id)
 stock_alert(df)
+add_product(all_category)
+remove_product()
